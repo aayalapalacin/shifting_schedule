e04fe47ab012ee3a1560d4ae57819df183a42d83
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2024-07-26 17:04:26 +0000

    consle

--- src/front/firebaseConfig.js
+++ src/front/firebaseConfig.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-console.log(process.env.FIREBASE_API_KEY,"process");
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
